9ca0860a583220c5c3aa009bf2df8c484b3edf13
Joshua Mckinnon <flxwedd@gmail.com>
2023-11-25 18:49:43 -0800

    Fix GetPartFromPort default part type

--- scripts/generateDefinitions.py
+++ scripts/generateDefinitions.py
@@ -37,8 +37,8 @@ def main():
     for part, _ in parts_data.items():
         single_overloads.append(f"((port: PortLike, partType: \"{part}\") -> {part})")
         multi_overloads.append(f"((port: PortLike, partType: \"{part}\") -> {{{part}}})")
-    single_overloads.append(f"((port: PortLike, partType: string) -> {part})")
-    multi_overloads.append(f"((port: PortLike, partType: string) -> {{{part}}})")
+    single_overloads.append(f"((port: PortLike, partType: string) -> {part_generator.default_part_name})")
+    multi_overloads.append(f"((port: PortLike, partType: string) -> {{{part_generator.default_part_name}}})")
     sep = '\n    & '
     content += f"declare GetPartFromPort: {sep.join(single_overloads)}\n"
     content += f"declare GetPartsFromPort: {sep.join(single_overloads)}\n"
